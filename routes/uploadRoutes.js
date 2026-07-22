c9f75c9d6f53e21fee99f626ae729382563fe0cc
hridaynath-patil <hridaynath.patil@zconsolutions.com>
2026-07-22 11:54:09 +0530

    Add uploads folder

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,28 +2,29 @@ const express = require("express");
 const router = express.Router();
 
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
 const { uploadFile } = require("../controllers/uploadController");
 
+const uploadDir = path.join(__dirname, "../uploads");
+
+// Create uploads folder automatically
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
-
     filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    }
-
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
 });
 
-const upload = multer({
-    storage: storage
-});
+const upload = multer({ storage });
 
-router.post(
-    "/upload",
-    upload.single("image"),
-    uploadFile
-);
+router.post("/upload", upload.single("image"), uploadFile);
 
 module.exports = router;
